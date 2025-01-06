a1c105816bd254133fa0f047173f288963f3e2b4
Jhens-Git <140466003+Jhens-Git@users.noreply.github.com>
2025-01-05 17:02:15 -0700

    Update aircraft.py with cash input now

--- aircraft.py
+++ aircraft.py
@@ -1,5 +1,5 @@
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-# Jamie Henson
+# Jamie Henson and Gabriel Scott
 # Prompt user to select the aircrafts for the senario
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
@@ -7,22 +7,28 @@ import csv
 
 def selectTeam(teamName):
     
-    # Initialize the team size and team array
+    # Initialize the team size, team array, and team cash
     teamSize = 0
     team = []
+    teamCash = 0
+
+    # Ask how much cash the team will have (maximum of $1 billion)
+    teamCash = select_cash(teamName)
     
     # Loop to build the team until the team size reaches 10
     while True:
     
         # Prompt user to select aircraft for the senario
-        aircraft_id = select_aircraft(teamName)
+        aircraft_id, aircraft_cost, min_price = select_aircraft(teamName, teamCash)
         
         # Prompt user to select quantity of aircrafts
-        aircraft_qty = select_qty(teamName, teamSize, aircraft_id)
+        aircraft_qty = select_qty(teamName, teamSize, aircraft_id, teamCash, aircraft_cost)
+        teamCash -= aircraft_qty * aircraft_cost
         teamSize += aircraft_qty
         
-        # Confirm the aircrafts added to the team
-        print(f"{aircraft_qty}x {aircraft_id} added to team {teamName}.")
+        # Confirm the aircrafts added to the team and remaining cash balance
+        print(f"{aircraft_qty} x {aircraft_id} added to team {teamName}.")
+        print(f"Team {teamName} has ${teamCash} million left.")
         
         # Add the aircraft to the team array
         for i in range(aircraft_qty):
@@ -31,6 +37,11 @@ def selectTeam(teamName):
         while True:
             # Check if the team size reaches 10
             if teamSize == 10:
+                print(f"Team {teamName} has reached their aircraft limit.")
+                break
+
+            if teamCash < min_price:
+                print(f"It is not possible for team {teamName} purchase any more aircraft.")
                 break
             
             # Ask user if they want more aircrafts
@@ -42,7 +53,7 @@ def selectTeam(teamName):
                 continue
         
         # If the team size reaches 10 or they finished building the team, break the loop
-        if teamSize == 10 or add == "no":
+        if teamSize == 10 or add == "no" or teamCash < min_price:
             break
         else:
             continue
@@ -50,41 +61,79 @@ def selectTeam(teamName):
     # Return the team array
     return team
 
-def select_aircraft(teamName):
+def select_cash(teamName):
 
     while True:
-        # Prompt user to pick an aircraft
-        aircraft_id = input(f"Enter the aircraft ID for team {teamName} (Example 'F22'): ")
-        
-        # Validate the aircraft ID
-        with open('SpecSheet.csv', mode = 'r') as file:
+        # Prompt user to enter a cash amount
+        cash = input(f"Enter amount of cash for team {teamName} (million USD): ")
+        cash_number = cash.isnumeric() # Checks if user inputted only numeric input
+             
+        if cash_number == 0:
+            print("\nInvalid input. Please enter only numbers!\n")
+        elif int(cash) > 1000:
+            print("\nInvalid input. Please enter a value less than or equal to 1 billion (1000).\n")
+        else:
+            break
+
+    cash = int(cash)
+            
+    return cash
+    
+def select_aircraft(teamName, teamCash):
+
+    with open(r'C:\Users\admir\OneDrive\VS Code\General\air_combat_sim\SpecSheet.csv') as file:
             csv_reader = csv.reader(file)
             next(csv_reader)  # Skip header
             valid_ids = [row[1] for row in csv_reader]
-            
-            # If the aircraft ID is not in the list of valid IDs, prompt the user to enter the ID again
-            if aircraft_id not in valid_ids:
+
+    with open(r'C:\Users\admir\OneDrive\VS Code\General\air_combat_sim\SpecSheet.csv') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip header
+        prices = [row[4] for row in csv_reader]
+
+    for i in range(len(prices)):
+        prices[i] = int(prices[i])
+
+    min_price = min(prices)
+
+    while True:
+        # Prompt user to pick an aircraft
+        aircraft_id = input(f"\nEnter an aircraft ID for team {teamName} (Example 'F22'): ")
+        
+        # Validate the aircraft ID
+        # If the aircraft ID is not in the list of valid IDs, prompt the user to enter the ID again
+        if aircraft_id not in valid_ids:
                 print("Invalid aircraft ID. Please try again.")
+        else:
+            # Extract the cost of the aircraft
+            for i in range(len(valid_ids)):
+                if aircraft_id == valid_ids[i]:
+                    aircraft_cost = prices[i]
+            if teamCash - aircraft_cost < 0:
+                print(f"Aircraft cost exceeds team {teamName}'s remaining cash. Please select a cheaper aircraft.")
             else:
                 break
-    
-    return aircraft_id
 
-def select_qty(teamName, teamSize, aircraft_id):
+    return aircraft_id, aircraft_cost, min_price
+
+def select_qty(teamName, teamSize, aircraft_id, teamCash, aircraft_cost):
     
     # If the input is invalid, prompt the user to enter the quantity again
     while True:
         # Prompt user to enter the number of aircrafts
-        aircraft_qty = input(f"Enter the number of {aircraft_id} for team {teamName} (Note, up to 10 allowed per team): ")
+        aircraft_qty = int(input(f"Enter the number of {aircraft_id} for team {teamName} (Note, up to 10 allowed per team): "))
         
         # Validate the aircraft quantity
-        if not aircraft_qty.isdigit():
+        if not str(aircraft_qty).isdigit():
             print("Invalid quantity. Please enter a number.")
-        elif int(aircraft_qty) < 1:
+        elif aircraft_qty < 1:
             print("Invalid quantity. Please enter a number greater than 0.")
-        elif int(aircraft_qty) + teamSize > 10:
+        elif aircraft_qty + teamSize > 10:
             print("Please enter a smaller number. The team has reached the limit of 10 aircrafts.")
         else:
-            break
+            if teamCash - (aircraft_qty * aircraft_cost) < 0:
+                print(f"Purchasing this quantity of the {aircraft_id} exceeds team {teamName}'s cash. Please lower the quantity purchased.")
+            else:
+                break
     
-    return int(aircraft_qty)
+    return aircraft_qty
