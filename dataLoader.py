6d8dc043f39f8a2c6fc39a6b96f1824b77dbff1b
Jhens-Git <140466003+Jhens-Git@users.noreply.github.com>
2025-01-09 09:34:26 -0700

    Update dataLoader.py Naming Update

--- dataLoader.py
+++ dataLoader.py
@@ -4,6 +4,7 @@
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 import csv
+import copy
 
 class Aircraft:
     # Store aircraft attributes
@@ -24,7 +25,7 @@ def loadData():
     Aircraft_dict = {}
     
     # Read the csv file and store aircrafts data in a dictionary for easy lookup
-    with open('Spec_Sheet.csv', mode = 'r') as file:
+    with open('SpecSheet.csv', mode = 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             row = {key.strip(): value.strip() for key, value in row.items()} # Strip spaces from column names and values
@@ -41,25 +42,31 @@ def loadData():
     
     return Aircraft_dict
 
-def getSpecs(IDs):
+def getSpecs(Team_IDs, Team_names):
     # Initialize team array and aircraft dictionary
     team = []
     Aircraft_dict = loadData()
     
-    for ID in IDs:
-        if ID in Aircraft_dict:
-            team.append(Aircraft_dict[ID])
-        else:
+    # Loop through all the IDs in the team, and assign each plane with attributes
+    for i in range(len(Team_IDs)):
+        ID = Team_IDs[i]
+        name = Team_names[i]
+        
+        if ID in Aircraft_dict: # ID exist in dictionary
+            aircraft_copy = copy.copy(Aircraft_dict[ID])  # Create a shallow copy (For some reason, aircraft dict is mutable)
+            aircraft_copy.name = name  # Update the name only in the copy
+            team.append(aircraft_copy)
+        else: # ID does not exist in dictionary
             print(f"Warning: {ID} not found in specifications sheet!")
     return team
 
 def displayAircraft():
     # Open the spec table of available aircraft
-    with open('Spec_Sheet.csv', mode = 'r') as file:
+    with open('SpecSheet.csv', mode = 'r') as file:
         csv_reader = csv.reader(file)
         header = next(csv_reader)
         # Print the 1st five columns of available aircraft
         print(f"{header[0]:<10} {header[1]:<10} {header[2]:<20} {header[3]:<10} {header[4]:<1} (million USD)")
-        print("-" * 60)
+        print("-" * 70)
         for row in csv_reader:
             print(f"{row[0]:<10} {row[1]:<10} {row[2]:<20} {row[3]:<10} {row[4]:<20}")
